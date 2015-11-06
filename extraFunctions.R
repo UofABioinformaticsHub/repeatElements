97eda9c89ebb8b339b84cb12b4ca84974e7be0be
steveped <stephen.pederson.au@gmail.com>
2015-11-06 15:42:23 +1030

    Nov 2015 revisions

--- extraFunctions.R
+++ extraFunctions.R
@@ -6,3 +6,15 @@ aster <- function(x) {
   out[which(x<0.001)] <- "***"
   out
 }
+
+capwords <- function(s, strict = FALSE, sep = "_") {
+  s <- gsub(sep, " ", s)
+  cap <- function(s) paste(toupper(substring(s, 1, 1)),
+{s <- substring(s, 2); if(strict) tolower(s) else s},
+sep = "", collapse = " " )
+sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+commas <- function(x){
+  format(x, digits = 0, big.mark = ",")
+}
