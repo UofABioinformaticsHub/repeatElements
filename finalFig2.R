36047cc146733029f0fd5d2cb3d4091ecfa6a50c
steveped <stephen.pederson.au@gmail.com>
2016-02-16 17:57:01 +1030

    Combined Fig2

---
+++ finalFig2.R
@@ -0,0 +1,37 @@
+library(grid)
+library(ggplot2)
+library(readr)
+
+# Exported from the file Fig2.Rnw
+p1 <- readRDS("p1.rds")
+p2 <- readRDS("p2.rds")
+
+fs <- 11
+p1$theme$strip.text$size <-fs
+p1$theme$axis.text.y$size <- fs
+p1$theme$axis.title.y$size <- fs + 1
+p1$theme$axis.title.y$angle <- 90
+p1$labels$y <- "Probability Estimate"
+p1$theme$legend.position <- "top"
+p1$theme$legend.text$size <- fs
+p1$theme$legend.title$size <- fs
+
+p2$theme$strip.text$size <- fs 
+p2$theme$axis.text.y$size <- fs
+p2$theme$axis.text.x$size <- fs
+p2$theme$axis.title.y$size <- fs + 1
+p2$theme$axis.title.x$size <- fs + 1
+p2$labels$x <- "Difference in logit(p)"
+p2$labels$y <- c()
+
+pdf("Figure2.pdf", 10, 10)
+grid.newpage()
+vp1 <- viewport(x=0.072, y=0.52, w=0.928, h=0.48, just=c("left","bottom"))
+vp2 <- viewport(x= 0, y=0, w=1, h=0.5, just=c("left","bottom"))
+#pushViewport(vp1)
+print(p1, vp=vp1)
+print(p2, vp=vp2)
+grid.text("A", x = 0.03, y = 0.96, default.units = "npc", gp = gpar(fontsize = fs + 4))
+grid.text("B", x = 0.03, y = 0.49, default.units = "npc", gp = gpar(fontsize = fs + 4))
+dev.off()
+
